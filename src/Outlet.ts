20f92318ff449ec0ee9e7fe0339668a99416b596
eric-yates <eric@lumenalabs.com>
2023-11-02 16:38:20 -0600

    Outlet throws with invalid sample rate

--- src/Outlet.ts
+++ src/Outlet.ts
@@ -1,17 +1,32 @@
 interface OutletArgs {
-	numChannels: number
+	numChannels?: number
+	sampleRate?: number
 }
 
 class Outlet {
 	private numChannels: number
+	private sampleRate: number
 
-	public constructor({ numChannels }: OutletArgs) {
-		if (numChannels <= 0 || !Number.isInteger(numChannels)) {
+	public constructor({ numChannels = 1, sampleRate = 100 }: OutletArgs) {
+		if (!isPositiveInteger(numChannels)) {
 			throw new Error('Invalid numChannels: must be a positive integer!')
 		}
 
+		if (!isPositiveNumber(sampleRate)) {
+			throw new Error('Invalid sampleRate: must be a positive number!')
+		}
+
 		this.numChannels = numChannels
+		this.sampleRate = sampleRate
 	}
+}
+
+const isPositiveNumber = (value: number) => {
+	return value > 0
+}
+
+const isPositiveInteger = (value: number) => {
+	return value > 0 && Number.isInteger(value)
 }
 
 export default Outlet
--- src/__tests__/behavioral/Outlet.test.ts
+++ src/__tests__/behavioral/Outlet.test.ts
@@ -4,8 +4,17 @@ import Outlet from '../../Outlet'
 export default class OutletTest extends AbstractSpruceTest {
 	@test()
 	protected static async outletThrowsWithInvalidNumChannels() {
-		assert.doesThrow(() => new Outlet({ numChannels: 0 }))
-		assert.doesThrow(() => new Outlet({ numChannels: -1 }))
-		assert.doesThrow(() => new Outlet({ numChannels: 2.5 }))
+		const errorMatcher = 'Invalid numChannels'
+		assert.doesThrow(() => new Outlet({ numChannels: 0 }), errorMatcher)
+		assert.doesThrow(() => new Outlet({ numChannels: -1 }), errorMatcher)
+		assert.doesThrow(() => new Outlet({ numChannels: 2.5 }), errorMatcher)
+	}
+
+	@test()
+	protected static async outletThrowsWithInvalidSampleRate() {
+		const errorMatcher = 'Invalid sampleRate'
+		assert.doesThrow(() => new Outlet({ sampleRate: 0 }), errorMatcher)
+		assert.doesThrow(() => new Outlet({ sampleRate: -1 }), errorMatcher)
+		assert.doesThrow(() => new Outlet({ sampleRate: -0.5 }), errorMatcher)
 	}
 }
