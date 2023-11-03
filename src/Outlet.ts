c6736d996d42cbf7a5216f385a0591145ff74216
eric-yates <eric@lumenalabs.com>
2023-11-03 17:22:35 -0600

    User input scaffolding complete

--- src/Outlet.ts
+++ src/Outlet.ts
@@ -1,25 +1,120 @@
-interface OutletArgs {
-	numChannels: number
+export interface OutletArgs {
+	name: string
+	type: string
+	channelCount: number
 	sampleRate: number
+	channelFormat: ChannelFormat
+	sourceId: string
+	manufacturer: string
+	unit: string
+	chunkSize: number
+	maxBuffered: number
 }
 
 class Outlet {
-	private numChannels: number
-	private sampleRate: number
+	protected name: string
+	protected type: string
+	protected channelCount: number
+	protected sampleRate: number
+	protected channelFormat: number
+	protected sourceId: string
+	protected manufacturer: string
+	protected unit: string
+	protected chunkSize: number
+	protected maxBuffered: number
 
-	public constructor({ numChannels, sampleRate }: OutletArgs) {
-		if (!isPositiveInteger(numChannels)) {
-			throw new Error('Invalid numChannels: must be a positive integer!')
-		}
+	private channelFormats: { [key in ChannelFormat]: number } = {
+		undefined: 0,
+		float32: 1,
+		double64: 2,
+		string: 3,
+		int32: 4,
+		int16: 5,
+		int8: 6,
+		int64: 7,
+	}
 
-		if (!isPositiveNumber(sampleRate)) {
-			throw new Error('Invalid sampleRate: must be a positive number!')
-		}
+	public constructor({
+		name,
+		type,
+		channelCount,
+		sampleRate,
+		channelFormat,
+		sourceId,
+		manufacturer,
+		unit,
+		chunkSize,
+		maxBuffered,
+	}: OutletArgs) {
+		this.validateChannelCount(channelCount)
+		this.validateSampleRate(sampleRate)
+		this.validateChannelFormat(channelFormat)
+		this.validateChunkSize(chunkSize)
+		this.validateMaxBuffered(maxBuffered)
 
-		this.numChannels = numChannels
+		this.name = name
+		this.type = type
+		this.channelCount = channelCount
 		this.sampleRate = sampleRate
+		this.channelFormat = this.channelFormats[channelFormat]
+		this.sourceId = sourceId
+		this.manufacturer = manufacturer
+		this.unit = unit
+		this.chunkSize = chunkSize
+		this.maxBuffered = maxBuffered
+	}
+
+	private validateChannelCount(channelCount: number) {
+		if (!isPositiveInteger(channelCount)) {
+			throw new Error(
+				`Invalid channelCount: must be a positive integer, not ${channelCount}!`
+			)
+		}
+	}
+
+	private validateSampleRate(sampleRate: number) {
+		if (!isPositiveNumber(sampleRate)) {
+			throw new Error(
+				`Invalid sampleRate: must be a positive number, not ${sampleRate}!`
+			)
+		}
+	}
+
+	private validateChannelFormat(channelFormat: string) {
+		const validChannelFormats = Object.keys(this.channelFormats).join(', ')
+		if (!(channelFormat in this.channelFormats)) {
+			throw new Error(
+				`Invalid channelFormat: must be one of ${validChannelFormats}, not ${channelFormat}!`
+			)
+		}
+	}
+
+	private validateChunkSize(chunkSize: number) {
+		if (!isPositiveIntegerOrZero(chunkSize)) {
+			throw new Error(
+				`Invalid chunkSize: must be a positive integer or zero, not ${chunkSize}!`
+			)
+		}
+	}
+
+	private validateMaxBuffered(maxBuffered: number) {
+		if (!isPositiveIntegerOrZero(maxBuffered)) {
+			throw new Error(
+				`Invalid maxBuffered: must be a positive integer or zero, not ${maxBuffered}!`
+			)
+		}
 	}
 }
+
+export type ChannelFormat =
+	| 'undefined'
+	| 'float32'
+	| 'double64'
+	| 'string'
+	| 'int32'
+	| 'int16'
+	| 'int8'
+	| 'int64'
 
 const isPositiveNumber = (value: number) => {
 	return value > 0
@@ -27,6 +122,10 @@ const isPositiveNumber = (value: number) => {
 
 const isPositiveInteger = (value: number) => {
 	return value > 0 && Number.isInteger(value)
+}
+
+const isPositiveIntegerOrZero = (value: number) => {
+	return value >= 0 && Number.isInteger(value)
 }
 
 export default Outlet
--- src/__tests__/behavioral/Outlet.test.ts
+++ src/__tests__/behavioral/Outlet.test.ts
@@ -1,43 +1,118 @@
 import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
-import Outlet from '../../Outlet'
+import Outlet, { ChannelFormat } from '../../Outlet'
 
 export default class OutletTest extends AbstractSpruceTest {
 	private static saneDefaults = {
-		numChannels: 3,
-		sampleRate: 100,
+		name: 'Muse S (2nd gen) - EEG',
+		type: 'EEG',
+		channelCount: 5,
+		sampleRate: 256,
+		channelFormat: 'float32',
+		sourceId: 'muse-eeg',
+		manufacturer: 'Interaxon Inc.',
+		unit: 'microvolts',
+		chunkSize: 0,
+		maxBuffered: 360,
+	}
+
+	private static validChannelFormats: ChannelFormat[] = [
+		'undefined',
+		'float32',
+		'double64',
+		'string',
+		'int32',
+		'int16',
+		'int8',
+		'int64',
+	]
+
+	@test()
+	protected static async outletHasRequiredProperties() {
+		const outlet = this.createOutletWithDefaults({})
+		const requiredProperties = Object.keys(this.saneDefaults)
+		for (let requiredProperty of requiredProperties) {
+			assert.isTrue(
+				requiredProperty in outlet,
+				`${requiredProperty} does not exist on Outlet instance`
+			)
+		}
+	}
+
+	@test()
+	protected static async outletPersistsValidSelectionFromChannelFormats() {
+		for (let validChannelFormat of this.validChannelFormats) {
+			const outlet = this.createOutletWithDefaults({
+				channelFormat: validChannelFormat,
+			})
+			assert.isNumber(outlet.getChannelFormat())
+		}
 	}
 
 	@test()
 	protected static async outletThrowsWithInvalidNumChannels() {
-		const errorMatcher = 'Invalid numChannels'
-		assert.doesThrow(
-			() => new Outlet({ ...this.saneDefaults, numChannels: 0 }),
-			errorMatcher
-		)
-		assert.doesThrow(
-			() => new Outlet({ ...this.saneDefaults, numChannels: -1 }),
-			errorMatcher
-		)
-		assert.doesThrow(
-			() => new Outlet({ ...this.saneDefaults, numChannels: 2.5 }),
-			errorMatcher
+		this.assertInvalidInputThrows(
+			'channelCount',
+			[0, 1.5, -1, -1.5],
+			'Invalid channelCount'
 		)
 	}
 
 	@test()
 	protected static async outletThrowsWithInvalidSampleRate() {
-		const errorMatcher = 'Invalid sampleRate'
-		assert.doesThrow(
-			() => new Outlet({ ...this.saneDefaults, sampleRate: 0 }),
-			errorMatcher
+		this.assertInvalidInputThrows(
+			'sampleRate',
+			[0, -1, -1.5],
+			'Invalid sampleRate'
 		)
-		assert.doesThrow(
-			() => new Outlet({ ...this.saneDefaults, sampleRate: -1 }),
-			errorMatcher
+	}
+
+	@test()
+	protected static async outletThrowsWithInvalidChannelFormat() {
+		this.assertInvalidInputThrows(
+			'channelFormat',
+			['float', 'int', 'double', 1],
+			'Invalid channelFormat'
 		)
-		assert.doesThrow(
-			() => new Outlet({ ...this.saneDefaults, sampleRate: -0.5 }),
-			errorMatcher
+	}
+
+	@test()
+	protected static async outletThrowsWithInvalidChunkSize() {
+		this.assertInvalidInputThrows(
+			'chunkSize',
+			[1.5, -1, -1.5],
+			'Invalid chunkSize'
 		)
+	}
+
+	@test()
+	protected static async outletThrowsWithInvalidMaxBuffered() {
+		this.assertInvalidInputThrows(
+			'maxBuffered',
+			[1.5, -1, -1.5],
+			'Invalid maxBuffered'
+		)
+	}
+
+	private static assertInvalidInputThrows(
+		argName: string,
+		invalidValues: any[],
+		errorMatcher: string
+	) {
+		for (let invalidValue of invalidValues) {
+			assert.doesThrow(
+				() => this.createOutletWithDefaults({ [argName]: invalidValue }),
+				errorMatcher
+			)
+		}
+	}
+
+	private static createOutletWithDefaults(args: any) {
+		return new SpyOutlet({ ...this.saneDefaults, ...args })
+	}
+}
+
+class SpyOutlet extends Outlet {
+	public getChannelFormat() {
+		return this.channelFormat
 	}
 }
