0ae8784df8a585e7dae076a5ecf5885d82d4ecdf
Taylor Romero <me@taylorrome.ro>
2023-11-15 16:33:45 -0700

    patch: rename

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as LiblslImpl } from './Liblsl'
 export * from './Liblsl'
 
-export { default as LiblslOutletImpl } from './LslOutlet'
+export { default as LslOutletImpl } from './LslOutlet'
 export * from './LslOutlet'
