4309c2109047f5dd432abb1827fb2b539e55018b
Taylor Romero <me@taylorrome.ro>
2023-11-15 16:29:39 -0700

    patch: export fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as LiblslImpl } from './Liblsl'
 export * from './Liblsl'
 
-export { default as LiblslOutlet } from './LslOutlet'
+export { default as LiblslOutletImpl } from './LslOutlet'
 export * from './LslOutlet'
