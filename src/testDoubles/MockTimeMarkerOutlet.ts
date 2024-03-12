09335a0a499d0c0d384bdc667de80c5ebfad4c87
eric-yates <eric@lumenalabs.com>
2024-03-12 14:38:34 -0600

    minor: update mock time marker outlet

--- src/testDoubles/MockTimeMarkerOutlet.ts
+++ src/testDoubles/MockTimeMarkerOutlet.ts
@@ -4,7 +4,9 @@ import { DurationMarker, LslSample, TimeMarkerOutlet } from '..'
 export default class MockTimeMarkerOutlet implements TimeMarkerOutlet {
 	public static instance: MockTimeMarkerOutlet
 	private didPushMarkers = false
-	private markers?: DurationMarker[]
+	private pushedMarkers?: DurationMarker[]
+	private didPushSamples = false
+	private pushedSamples?: LslSample[]
 
 	public constructor() {
 		MockTimeMarkerOutlet.instance = this
@@ -12,11 +14,36 @@ export default class MockTimeMarkerOutlet implements TimeMarkerOutlet {
 
 	public stop(): void {}
 	public destroy(): void {}
-	public pushSample(_sample: LslSample): void {}
+
+	public pushSample(sample: LslSample): void {
+		if (!this.didPushSamples) {
+			this.didPushSamples = true
+			this.pushedSamples = []
+		}
+		this.pushedSamples?.push(sample)
+	}
 
 	public async pushMarkers(markers: DurationMarker[]): Promise<void> {
 		this.didPushMarkers = true
-		this.markers = markers
+		this.pushedMarkers = markers
+	}
+
+	public assertDidPushSamples(samples?: LslSample[]) {
+		assert.isTrue(
+			this.didPushSamples,
+			`Expected to have pushed samples but didn't. Try 'outlet.pushSample(...)'`
+		)
+
+		if (samples) {
+			assert.isEqualDeep(this.pushedSamples, samples)
+		}
+	}
+
+	public assertDidNotPushSamples() {
+		assert.isFalse(
+			this.didPushSamples,
+			`Expected to NOT have pushed samples but did. Try 'outlet.pushSample(...)'`
+		)
 	}
 
 	public assertDidPushMarkers(markers?: DurationMarker[]) {
@@ -26,7 +53,7 @@ export default class MockTimeMarkerOutlet implements TimeMarkerOutlet {
 		)
 
 		if (markers) {
-			assert.isEqualDeep(this.markers, markers)
+			assert.isEqualDeep(this.pushedMarkers, markers)
 		}
 	}
 
