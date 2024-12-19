992a70c19b25cc310cd4c9ead62dfb1a5833b7e0
ericthecurious <hello@ericthecurious.com>
2024-12-19 03:20:06 -0600

    patch: update fake

--- src/testDoubles/FakeLslOutlet.ts
+++ src/testDoubles/FakeLslOutlet.ts
@@ -5,12 +5,14 @@ import { LslSample } from '../nodeLsl.types'
 export default class FakeLslOutlet implements LslOutlet {
     public static constructorInfo?: StreamInfo
     public static constructorOptions?: LslOutletOptions
+    public static callsToConstructor: CallToConstructor[] = []
     public static callsToPushSample: LslSample[] = []
     public static numCallsToDestroy = 0
 
     public constructor(info: StreamInfo, options: LslOutletOptions) {
         FakeLslOutlet.constructorInfo = info
         FakeLslOutlet.constructorOptions = options
+        FakeLslOutlet.callsToConstructor.push({ info, options })
     }
 
     public destroy() {
@@ -22,8 +24,15 @@ export default class FakeLslOutlet implements LslOutlet {
     }
 
     public static resetTestDouble() {
+        FakeLslOutlet.constructorInfo = undefined
         FakeLslOutlet.constructorOptions = undefined
+        FakeLslOutlet.callsToConstructor = []
         FakeLslOutlet.callsToPushSample = []
         FakeLslOutlet.numCallsToDestroy = 0
     }
+}
+
+export interface CallToConstructor {
+    info: StreamInfo
+    options: LslOutletOptions
 }
