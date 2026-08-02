27581845a1b45c5f274add8c22941dd751674e8b
eric-yates <hello@ericthecurious.com>
2026-08-01 23:38:03 -0700

    patch: loads with json specific options once

--- src/__tests__/impl/LslJsonOutlet.test.ts
+++ src/__tests__/impl/LslJsonOutlet.test.ts
@@ -1,6 +1,7 @@
 import { test, assert } from '@neurodevs/node-tdd'
 
 import LslJsonOutlet, { JsonOutlet } from '../../impl/LslJsonOutlet.js'
+import FakeLslOutlet from '../../testDoubles/LslOutlet/FakeLslOutlet.js'
 import AbstractPackageTest from '../AbstractPackageTest.js'
 
 export default class LslJsonOutletTest extends AbstractPackageTest {
@@ -9,12 +10,27 @@ export default class LslJsonOutletTest extends AbstractPackageTest {
     protected static async beforeEach() {
         await super.beforeEach()
 
-        this.instance = this.LslJsonOutlet()
+        this.setFakeLslOutlet()
+
+        this.instance = await this.LslJsonOutlet()
     }
 
     @test()
     protected static async createsInstance() {
         assert.isTruthy(this.instance, 'Failed to create instance!')
+    }
+
+    @test()
+    protected static async loadsWithJsonSpecificOptionsOnce() {
+        assert.isEqualDeep(FakeLslOutlet.callsToConstructor[0], {
+            name: `JSON (json-1)`,
+            type: 'JSON',
+            sourceId: 'json-1',
+            channelNames: ['JSON'],
+            channelFormat: 'string',
+            sampleRateHz: 0,
+            chunkSize: 1,
+        })
     }
 
     private static LslJsonOutlet() {
--- src/impl/LslJsonOutlet.ts
+++ src/impl/LslJsonOutlet.ts
@@ -1,10 +1,26 @@
+import LslStreamOutlet from './LslStreamOutlet.js'
+
 export default class LslJsonOutlet implements JsonOutlet {
     public static Class?: JsonOutletConstructor
 
     protected constructor(_options?: JsonOutletOptions) {}
 
     public static async Create(options?: JsonOutletOptions) {
+        await this.LslStreamOutlet(options)
         return new (this.Class ?? this)(options)
+    }
+
+    private static LslStreamOutlet(options?: JsonOutletOptions) {
+        return LslStreamOutlet.Create({
+            name: `JSON (json-1)`,
+            type: 'JSON',
+            sourceId: 'json-1',
+            channelNames: ['JSON'],
+            channelFormat: 'string',
+            sampleRateHz: 0,
+            chunkSize: 1,
+            ...options,
+        })
     }
 }
 
